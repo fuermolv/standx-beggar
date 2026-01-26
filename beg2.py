00ec58ff9570b0eb44af3d213a990e040009a21f
lvyu <lvyu@DESKTOP-9K7BOMM.localdomain>
2026-01-26 14:54:39 +0800

    update

--- beg2.py
+++ beg2.py
@@ -96,6 +96,7 @@ def main(position, auth):
                             break
                         time.sleep(1)
             if long_diff_bps <= MIN_BPS or long_diff_bps >= MAX_BPS or short_diff_bps <= MIN_BPS or short_diff_bps >= MAX_BPS:
+                logger.info(f'pos:{position}, mark_price: {mark_price}, best_ask: {best_ask_price}, best_bid: {best_bid_price}, long order bps: {long_diff_bps}, short order bps: {short_diff_bps}')
                 cancel_orders(auth, [cid for cid in [order_dict['long_cl_ord_id'], order_dict['short_cl_ord_id']] if cid])
                 order_dict = None
                 clean_orders(auth)
